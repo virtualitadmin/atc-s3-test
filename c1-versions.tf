37d195ae011aa3c614ced2aadca1783006c593a5
MikeS <mikes@metsi.co.uk>
2021-10-07 16:20:34 +0100

    TF S3 Website c1-version updated

--- c1-versions.tf
+++ c1-versions.tf
@@ -11,7 +11,6 @@ terraform {
 # Provider Block
 provider "aws" {
   region  = var.aws_region
-  profile = "default"
 }
 /*
 Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
